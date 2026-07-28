1a1b2a0f585b45923f894414437d64386b78962d
Mattiplex <cariwowowong@gmail.com>
2026-07-29 00:47:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+function updatetime() {
+  var currentTime = new Date().toLocaleString();
+  var timetext = document.querySelector('#timeElement');
+  timetext.innerHTML = currentTime;
+  console.log(currentTime);
+}
+
+setInterval(updatetime, 1000); 
+
+dragElement(document.getElementById("welcomescreen"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+var welcomescreen = document.querySelector(#welcomescreen)
+var welcomescreen = document.queryselector(#welcomeclose)
+var welcomescreen = document.queryselector(#welcomeopen)
+function closeWindow(elmnt) {
+  elmnt.style.display = "none"
+}
+function openWindow(elmnt) { 
+elmnt.style.display = "flex"
+biggestIndex++; //Increment biggestIndex by 1 
+elmnt.style.zIndex = biggestIndex;
+topBar.style.zIndex = biggest +1;
+elmnt.style.top = elmnt.style.left
+}
+
+welcomeScreenclose.addEventListener("click",function() {
+ closeWindow(welcomeScreen);
+});
+welcomeScreenopen.addEventListener("click",function() {
+ openWindow(welcomeScreen);
+});
+
