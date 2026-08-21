bd6c112e26800123b5f64375724e4a2344183843
Mattiplex <cariwowowong@gmail.com>
2026-08-22 02:07:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -128,6 +128,8 @@ function handleWindowTap(element) {
   deselectIcon(selectedIcon)
 }
 
+// notes app function
+
 let notes =[]
 let editingNoteId = null
 
@@ -265,9 +267,9 @@ renderNotes()
   })
 })
 
+// special effects and cool open source stuff
 
-
-// <![CDATA[
+// rainbow text effect
 var speed=100; // speed colours change, 1 second = 1000
 var glow=5; // can be set from '0' for no glow, to 10
 var raincol=new Array("#ff0000", "#ff5500", "#ffaa00", "#ffff00", "#aaff00", "#55ff00", "#00ff00", "#00ff55", "#00ffaa", "#00ffff", "#00aaff", "#0055ff", "#0000ff", "#5500ff", "#aa00ff", "#ff00ff", "#ff00aa", "#ff0055"); // change the colours if you want to
@@ -318,7 +320,7 @@ function raining() {
   }
   raincnt++;
 }
-// ]]>
+// rainbow cursor 
 
  import { rainbowCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
 
@@ -335,3 +337,78 @@ function relay(e) {
 window.addEventListener('mousemove', relay);
 window.addEventListener('touchmove', relay, { passive: true });
 window.addEventListener('touchstart', relay, { passive: true });
+
+//rolling text effect
+
+var text=Array("20% Cooler In 10 Seconds Flat",
+   "time to take out the adorable trash", 
+   "Did you note how the web-address was turned into a hyperlink?", 
+   "Another free effect "+String.fromCharCode(169)+"2014 onwards RV's free DHTML effects",
+   "Check back from time to time to get the latest sexy scripts...", 
+   "...free to use on your web-site or blog. How good is that?"); // replace this with an array of your own text
+var delay=10; // seconds between each new text item
+var speed=50; // speed of the scroll
+
+/***************************\
+*    Rolling Text Effect    *
+* (c)2014+ mf2fm web-design *
+*  http://www.mf2fm.com/rv  *
+* DON'T EDIT BELOW THIS BOX *
+\***************************/
+var diddly=new Array();
+var rolltop;
+var referee=0;
+delay*=1000;
+
+if (typeof('addRVLoadEvent')!='function') function addRVLoadEvent(funky) {
+  var oldonload=window.onload;
+  if (typeof(oldonload)!='function') window.onload=funky;
+  else window.onload=function() {
+    if (oldonload) oldonload();
+    funky();
+  }
+}
+
+addRVLoadEvent(prepare_to_go);
+
+function prepare_to_go() {
+  var i, r;
+  var r=document.getElementById("rolling");
+  rolltop=r.clientHeight;
+  while (r.childNodes.length) r.removeChild(r.childNodes[0]);
+  r.style.position="relative";
+  r.style.overflow="hidden";
+  r.appendChild(document.createTextNode(String.fromCharCode(160)));
+  for (i=0; i<text.length; i++) {
+    diddly[i]=document.createElement("div");
+	diddly[i].style.position="absolute";
+	diddly[i].style.top=rolltop+"px";
+	diddly[i].style.left="0px";
+	if (text[i].indexOf("www")!=-1) slinkit(diddly[i], text[i]);
+	else diddly[i].appendChild(document.createTextNode(text[i]));
+	r.appendChild(diddly[i]);
+  }
+  rolling_in_the_hay();
+}
+
+function rolling_in_the_hay() {
+  var i, j;
+  j=referee%text.length;
+  for (i=rolltop; i>=0; i--) setTimeout("diddly["+j+"].style.top='"+i+"px'", speed*(rolltop-i));
+  for (i=-1; i>-rolltop; i--) setTimeout("diddly["+j+"].style.top='"+i+"px'", delay-speed*i);
+  ++referee;
+  setTimeout('rolling_in_the_hay()', delay);
+}
+
+function slinkit(r, t) {
+  var a, s, e;
+  s=t.indexOf("www");
+  e=t.indexOf(" ", s);
+  if (e==-1) e=t.length;
+  r.appendChild(document.createTextNode(t.substring(0, s)));
+  a=document.createElement("a");
+  a.href="http://"+t.substring(s, e);
+  a.appendChild(document.createTextNode(t.substring(s, e)));
+  r.appendChild(a);
+  r.appendChild(document.createTextNode(t.substring(e)));
+}
