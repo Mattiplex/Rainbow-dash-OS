06001ab1a74e62dc9e22b8e43febf41e51d50f37
Mattiplex <cariwowowong@gmail.com>
2026-09-06 02:32:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -437,3 +437,40 @@ function slinkit(r, t) {
   r.appendChild(a);
   r.appendChild(document.createTextNode(t.substring(e)));
 }
+
+// MUSIC PLAYER BY GLENTHEMES
+
+// you may study the code to see how I did things but please do not steal or claim as your own!
+// learning resource 01: https://www.w3schools.com/jsref/dom_obj_audio.asp
+// learning resource 02: https://www.w3schools.com/tags/tag_audio.asp
+
+$(document).ready(function(){
+    $(".playy").click(function() {
+        $(".pausee").show();
+        $(".playy").hide();
+    });
+
+    $(".pausee").click(function() {
+        $(".playy").show();
+        $(".pausee").hide();
+    });
+});
+
+function songstart() {
+var harmonia = document.getElementById("tune");
+    if (harmonia.paused) {
+        harmonia.play();
+    } else { 
+        harmonia.pause();
+    }
+}
+
+window.songstart = songstart; // addition to make it function
+
+$(document).ready(function(){
+var owari = document.getElementById("tune");
+owari.onended = function() {
+    $(".playy").show();
+    $(".pausee").hide();
+};
+});
