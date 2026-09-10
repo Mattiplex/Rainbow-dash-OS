fb99983cb0ef3560283f495210250bd008d6225b
Mattiplex <cariwowowong@gmail.com>
2026-09-11 01:39:44 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -437,80 +437,156 @@ function slinkit(r, t) {
   r.appendChild(a);
   r.appendChild(document.createTextNode(t.substring(e)));
 }
-
-// MUSIC PLAYER BY GLENTHEMES
-
-// you may study the code to see how I did things but please do not steal or claim as your own!
-// learning resource 01: https://www.w3schools.com/jsref/dom_obj_audio.asp
-// learning resource 02: https://www.w3schools.com/tags/tag_audio.asp
-
-$(document).ready(function(){
-    $(".playy").click(function() {
-        $(".pausee").show();
-        $(".playy").hide();
-    });
-
-    $(".pausee").click(function() {
-        $(".playy").show();
-        $(".pausee").hide();
-    });
-});
-
-function songstart() {
-var harmonia = document.getElementById("tune");
-    if (harmonia.paused) {
-        harmonia.play();
-    } else { 
-        harmonia.pause();
-    }
-}
-
-window.songstart = songstart; // addition to make it function
-window.Nextsong = Nextsong;
-window.Pervsong = Pervsong;
-
-$(document).ready(function(){
-var owari = document.getElementById("tune");
-owari.onended = function() {
-    $(".playy").show();
-    $(".pausee").hide();
-};
-});
-
-//my custom addition 
-var  songs = [
-  "Cheel - Blue Dream (Original) - Free copyright music.mp3",
-  "Nyan Cat [original].mp3",
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+// Specify globally used values
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+// Create the audio element for the player
+let curr_track = document.createElement("audio");
+// Define the list of tracks that have to be played
+let track_list = [
+	{
+		name: "NYan Cat [original]",
+		artist: "PRguitarman, Daniwell",
+		image:
+			"https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGg3emlmOWs4djVxaHBucnBwZGxud3ZnaG5seW0zbG5pNGxuZnhxeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/BSx6mzbW1ew7K/giphy.gif",
+		path: "Nyan Cat [original].mp3"
+	},
+	{
+		name: "Blue dream",
+		artist: "Cheel",
+		image:"https://cdn-images.dzcdn.net/images/cover/65626324e93cc9d56070464d230aaf8e/0x1900-000000-80-0-0.jpg",
+		path: "Cheel - Blue Dream (Original) - Free copyright music.mp3"
+	}
 ];
-
-var currentSong = 0;
-
-function Nextsong() {
-  ++currentSong;
-  if (currentSong >= songs.length) {
-    currentSong = 0;
-  }
-
-  var harmonia = document.getElementById("tune");
-
-  harmonia.src=songs[CurrentSong];
-  harmonia.play();
-
-  $(".play").hide();
-  $(".pause")
+//
+//
+function loadTrack(track_index) {
+	// Clear the previous seek timer
+	clearInterval(updateTimer);
+	resetValues();
+	// Load a new track
+	curr_track.src = track_list[track_index].path;
+	curr_track.load();
+	// Update details of the track
+	track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+	track_name.textContent = track_list[track_index].name;
+	track_artist.textContent = track_list[track_index].artist;
+	now_playing.textContent =
+		"PLAYING " + (track_index + 1) + " OF " + track_list.length;
+	// Set an interval of 1000 milliseconds
+	// for updating the seek slider
+	updateTimer = setInterval(seekUpdate, 1000);
+	// Move to the next track if the current finishes playing
+	// using the 'ended' event
+	curr_track.addEventListener("ended", nextTrack);
+	// Apply a random background color
+	random_bg_color();
+}
+// Function to reset all values to their default
+function resetValues() {
+	curr_time.textContent = "00:00";
+	total_duration.textContent = "00:00";
+	seek_slider.value = 0;
 }
 
-function Pervsong() {
-  currentSong--;
-  if (currentSong < 0) {
-    currentSong = songs.length - 1;
-  }
-
-  var harmonia = document.getElementById("tune");
-
-  harmonia.src=songs[CurrentSong];
-  harmonia.play();
-
-  $(".play").hide();
-  $(".pause")
+function playpauseTrack() {
+	// Switch between playing and pausing
+	// depending on the current state
+	if (!isPlaying) playTrack();
+	else pauseTrack();
 }
+
+function playTrack() {
+	// Play the loaded track
+	curr_track.play();
+	isPlaying = true;
+	// Replace icon with the pause icon
+	playpause_btn.innerHTML = '<i class="fa fa-pause fa-lg"></i>';
+}
+
+function pauseTrack() {
+	// Pause the loaded track
+	curr_track.pause();
+	isPlaying = false;
+	// Replace icon with the play icon
+	playpause_btn.innerHTML = '<i class="fa fa-play fa-lg"></i>';
+}
+
+function nextTrack() {
+	// Go back to the first track if the
+	// current one is the last in the track list
+	if (track_index < track_list.length - 1) track_index += 1;
+	else track_index = 0;
+	// Load and play the new track
+	loadTrack(track_index);
+	playTrack();
+}
+
+function prevTrack() {
+	// Go back to the last track if the
+	// current one is the first in the track list
+	if (track_index > 0) track_index -= 1;
+	else track_index = track_list.length - 1;
+	// Load and play the new track
+	loadTrack(track_index);
+	playTrack();
+}
+
+function seekTo() {
+	// Calculate the seek position by the
+	// percentage of the seek slider
+	// and get the relative duration to the track
+	seekto = curr_track.duration * (seek_slider.value / 100);
+	// Set the current track position to the calculated seek position
+	curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+	// Set the volume according to the
+	// percentage of the volume slider set
+	curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+	let seekPosition = 0;
+	// Check if the current track duration is a legible number
+	if (!isNaN(curr_track.duration)) {
+		seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+		seek_slider.value = seekPosition;
+		// Calculate the time left and the total duration
+		let currentMinutes = Math.floor(curr_track.currentTime / 60);
+		let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+		let durationMinutes = Math.floor(curr_track.duration / 60);
+		let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+		// Add a zero to the single digit time values
+		if (currentSeconds < 10) {
+			currentSeconds = "0" + currentSeconds;
+		}
+		if (durationSeconds < 10) {
+			durationSeconds = "0" + durationSeconds;
+		}
+		if (currentMinutes < 10) {
+			currentMinutes = "0" + currentMinutes;
+		}
+		if (durationMinutes < 10) {
+			durationMinutes = "0" + durationMinutes;
+		}
+		// Display the updated duration
+		curr_time.textContent = currentMinutes + ":" + currentSeconds;
+		total_duration.textContent = durationMinutes + ":" + durationSeconds;
+	}
+}
+// Load the first track in the tracklist
+loadTrack(track_index);
