824cb415f2c823f07bec70d65d183cdb9389ccc5
Mattiplex <cariwowowong@gmail.com>
2026-09-11 01:53:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -437,6 +437,16 @@ function slinkit(r, t) {
   r.appendChild(a);
   r.appendChild(document.createTextNode(t.substring(e)));
 }
+
+window.playpauseTrack = playpauseTrack;
+window.nextTrack = nextTrack;
+window.prevTrack = prevTrack;
+window.seekto = seekTo;
+window.setvolume = setVolume;
+window.seekUpdate = seekUpdate;
+
+
+
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
